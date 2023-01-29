7095b5e173e86cc7537767cb1135586dda78585a
danneba <danielkassahun2@gmail.com>
2023-01-29 14:55:31 +0300

    apollo configuration

---
+++ src/Plugins/apollo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import App from "./App";
+
+const client = new ApolloClient({
+  uri: "",
+  cache: new InMemoryCache(),
+});
+
+// Supported in React 18+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
