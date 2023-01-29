047065be80ddda11e2ebf7f1fb98f1edb28f0dfa
danneba <danielkassahun2@gmail.com>
2023-01-29 14:44:05 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <p className="text-red-500 text-3xl">This is text</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
