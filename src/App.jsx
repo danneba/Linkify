162d1a943f91df1ad88fcfbf207f8bfa2b2b4761
Ephrem Demlew <ephrem.demlew@hahu.jobs>
2023-02-08 09:14:51 +0300

    adding router

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="w-full ">
       <div>
         <p className="text-red-500 text-3xl">This is text</p>
+        <div className="w-full bg-black ">
+          <img src={reactLogo} className="w-full object-cover" />
+        </div>
       </div>
     </div>
   );
