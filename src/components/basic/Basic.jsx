234de10378b8254317a8c13364c66b6b855583a7
danneba <danielkassahun2@gmail.com>
2023-02-08 07:48:21 +0300

    done on the main page

---
+++ src/components/basic/Basic.jsx
@@ -0,0 +1,12 @@
+import React from " react";
+import logo from "./assets/Link.svg";
+
+function basic() {
+  return (
+    <div className="flex w-full h-full">
+      <div className="flex">
+        <img src="firstSection" alt="there is alternative" />
+      </div>
+    </div>
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "nature-light": "url('./assets/main.png')",
+        "avatar-pic": "url('./assets/avatar.jpg')",
+        "search-pic": "url('./assets/search.png')",
+        "trending-pic": "url('./assets/trending.png')",
+        "trend-pic": "url('./assets/trend.png')",
+        "tren-pic": "url('./assets/tren.png')",
+      },
+      color: {
+        primary: "#001235",
+        "primary-bg": "#ff8700",
+      },
+    },
   },
   plugins: [],
 };
