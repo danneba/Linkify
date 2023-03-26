94e72411b54ace8ad3ac650c68d4a5f82cfcc09d
Ephrem Demlew <ephrem.demlew@hahu.jobs>
2023-03-26 11:30:36 +0300

    added the route guard and lifelet map

--- src/pages/home/AboutUs.jsx
+++ src/pages/home/AboutUs.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { TiTick } from "react-icons/ti";
-
+import footerBG from "../../assets/footerBG.png";
 
 import eph from "../../assets/eph.jpg";
 import dan from "../../assets/dan.jpg";
 import html from "../../assets/html.png";
-
 
 import {
   UilUser,
@@ -25,9 +24,13 @@ import {
 function About() {
   return (
     <div className="flex flex-col">
-      <div className="flex relative  bg-about gap-32 py-2 h-[700px] justify-center ">
-        {/* <img src={curves} className="w-full h-96 "/> */}
-        <div className="flex flex-col gap-4 justify-center items-center relative">
+      <div className="flex relative h-[500px]">
+        <img
+          src={footerBG}
+          className="h-[500px] absolute w-full top-0 rotate-180"
+          alt=""
+        />
+        <div className="flex flex-col gap-4 justify-center items-center relative w-full">
           <h1 className="flex text-white text-6xl font-semibold">
             About Linkify Events:
           </h1>
@@ -40,7 +43,7 @@ function About() {
         </div>
       </div>
       <div className="flex flex-col mt-16 gap-8 justify-center items-center">
-          <p className="flex text-pretify font-semibold text-7xl ">Our Mission</p>
+        <p className="flex text-pretify font-semibold text-7xl ">Our Mission</p>
         <p className="flex text-pretify font-semibold text-4xl w-[60%] text-center">
           Our mission is to provide a seamless and convenient ticketing
           experience for all of our users.
@@ -85,7 +88,10 @@ function About() {
           </p>
         </div>
         <div className="flex mt-12  ml-16">
-          <Link to="/login"  className="flex py-4 px-8 bg-about text-white text-lg font-semibold justify-center items-center">
+          <Link
+            to="/login"
+            className="flex py-4 px-8 bg-about text-white text-lg font-semibold justify-center items-center"
+          >
             Create Event
           </Link>
         </div>
@@ -95,35 +101,49 @@ function About() {
           </p>
         </div>
         <div className="flex justify-between gap-x-44 mt-8">
-          <div className="flex flex-col relative shadow-sm hover:shadow-xl py-6 border px-6 items-center justify-center">
-            <img src={dan} className="rounded-full w-44 h-44 mb-6" />
-            <p className="flex ml-6 mt-4 text-2xl font-semibold">Daniel Kassahun</p>
+          <a
+            href="https://www.linkedin.com/in/daniel-kassahun/"
+            className="flex flex-col cursor-pointer relative shadow-sm hover:shadow-xl py-14 rounded-lg border px-6 items-center justify-center"
+          >
+            <img src={dan} className="rounded-full w-44  mb-6 bg-cover" />
+            <p className="flex ml-6 mt-4 text-2xl font-semibold">
+              Daniel Kassahun
+            </p>
             <p className="flex ml-10  text-pretify text-lg">Co-founder</p>
             <p className="w-64 text-center">
               Responsible for Operating, designing, developing and testing
               Linkify Events.
             </p>
             <img src={html} className="w-8 absolute top-1 right-3" />
-          </div>
-          <div className="flex flex-col relative shadow-sm hover:shadow-xl py-6 border px-6 items-center justify-center">
-            <img src={eph} className="rounded-full w-44 h-44 mb-6" />
-            <p className="flex ml-6 mt-4 text-2xl font-semibold">Ephrem Demlew</p>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ephremdemlew/"
+            className="flex flex-col cursor-pointer relative shadow-sm hover:shadow-xl py-14 rounded-lg border px-6 items-center justify-center"
+          >
+            <img src={eph} className="rounded-full w-44  mb-6" />
+            <p className="flex ml-6 mt-4 text-2xl font-semibold">
+              Ephrem Demlew
+            </p>
             <p className="flex ml-10  text-pretify text-lg">Co-founder</p>
             <p className="w-64 text-center">
               Responsible for Operating, designing, developing and testing
               Linkify Events.
             </p>
             <img src={html} className="w-8 absolute top-1 right-3" />
-
-          </div>
+          </a>
         </div>
       </div>
-      <div className="flex flex-col w-full bg-about py-8 justify-center items-center mt-32"> 
-           <p className="text-white text-2xl ">Event ticketing just got easier</p>
-           <p className="text-white text-5xl font-semibold ">Create your events in 2 minutes</p>
-           <div className="flex mt-12  ">
-          <Link to="/login"  className="flex py-4 px-8 bg-mainRed text-white text-lg font-semibold justify-center items-center">
-          Create Event
+      <div className="flex flex-col w-full bg-about py-8 justify-center items-center mt-32">
+        <p className="text-white text-2xl ">Event ticketing just got easier</p>
+        <p className="text-white text-5xl font-semibold ">
+          Create your events in 2 minutes
+        </p>
+        <div className="flex mt-12  ">
+          <Link
+            to="/login"
+            className="flex py-4 px-8 bg-mainRed text-white text-lg font-semibold justify-center items-center"
+          >
+            Create Event
           </Link>
         </div>
       </div>
--- src/pages/home/ContactUs.jsx
+++ src/pages/home/ContactUs.jsx
@@ -12,12 +12,12 @@ import photoMain from "../../assets/photo.jpg";
 import curves from "../../assets/splitNegative.svg";
 import heart from "../../assets/download.png";
 import address from "../../assets/friend.jpg";
+import footerBG from "../../assets/footerBG.png";
 
 import phone from "../../assets/phone.avif";
 import message from "../../assets/message.jpg";
 import habesha from "../../assets/habesha.jpg";
 import two from "../../assets/two.jpg";
-
 
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
@@ -35,90 +35,192 @@ import {
   UilPlusCircle,
   UilAngleDoubleDown,
 } from "@iconscout/react-unicons";
+import "leaflet/dist/leaflet.css";
+
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 function Contact() {
- 
+  const position = [8.980603, 38.757759];
   return (
     <div className="flex flex-col">
-       <div className="flex relative  bg-contact gap-24 py-2 h-[700px] justify-center "> 
-           {/* <img src={curves} className="w-full h-96 "/> */}
-           <div className="flex w-20 h-20 rounded-full mt-96  mr-24 ">
-           <img src={habesha} className="flex rounded-full shadow-sm border-2 w-20"/>
-             
-           </div>
-           <div className="flex w-16 h-16 rounded-full mt-64 mr-24 ">
-           <img src={message} className="flex rounded-full shadow-sm border-2 w-20"/>
-             
-           </div>
-           <div className="flex flex-col gap-4 justify-center items-center">
-           <h1 className="flex text-white text-7xl font-semibold">Contact</h1>
-           <p className="text-white w-96">let's start something great together. Get in touch with one of the team today</p>
-           <UilAngleDoubleDown className=" text-white" width="52" size="140"/>
-           </div>
-           <div className="flex w-16 h-16 bg-white rounded-full justify-center mt-64  items-center ">
-             <img src={heart} className="flex w-20"/>
-           </div>
-           <div className="flex w-20 h-20 rounded-full mt-96 ">
-           <img src={phone} className="flex rounded-full shadow-sm border-2 w-20"/>
-             
-           </div>
-           <div className="flex w-24 h-24  absolute -bottom-12  rounded-full  ">
-           <img src={two} className="flex rounded-full shadow-sm border-2 border-[#ffafcc] w-24"/>
-             
-           </div>
-       </div>
-       <div className="flex justify-between "> 
-          <div className="flex flex-col ml-24 gap-8  justify-center items-center ">
-            <p className=" text-4xl font-medium ">Get in touch</p>
-            <div className="flex flex-col gap-4">
+      <div className="flex relative h-[500px]">
+        <img
+          src={footerBG}
+          className="h-[500px] absolute w-full top-0 rotate-180"
+          alt=""
+        />
+        {/* <img src={curves} className="w-full h-96 "/> */}
+        <div className="flex absolute w-20 h-20 -bottom-3 rounded-full left-56">
+          <img
+            src={habesha}
+            className="flex rounded-full shadow-sm border-2 bg-no-repeat bg-cover "
+          />
+        </div>
+        <div className="flex absolute w-10 h-10 rounded-full top-28 left-32">
+          <img
+            src={message}
+            className="flex rounded-full shadow-sm border-2 "
+          />
+        </div>
+        <div className="flex flex-col gap-4 justify-center items-center mt-20 z-50 w-full">
+          <h1 className="flex text-white text-7xl font-semibold">Contact</h1>
+          <p className="text-white w-96">
+            let's start something great together. Get in touch with one of the
+            team today
+          </p>
+          <UilAngleDoubleDown className=" text-white" width="52" size="140" />
+        </div>
+        <div className="flex absolute w-14 h-14 top-20 rounded-full right-96">
+          <img src={heart} className="flex w-20" />
+        </div>
+        <div className="flex absolute w-20 h-20 -bottom-10 rounded-full right-64 ">
+          <img
+            src={phone}
+            className="flex rounded-full shadow-sm border-2 w-20"
+          />
+        </div>
+        <div className="flex absolute w-20 h-20 top-60 rounded-full right-[600px]">
+          <img
+            src={two}
+            className="flex rounded-full shadow-sm border-2 border-[#ffafcc] bg-cover w-24"
+          />
+        </div>
+      </div>
+      <div className="flex justify-center items-start">
+        <div className="w-full flex flex-col mt-24 gap-8  justify-center items-end   ">
+          <div className="w-[90%]  flex flex-col gap-y-10">
+            <p className="text-center text-4xl font-medium font-Lobster text-mainRed">
+              Get in touch
+            </p>
+            <div className="flex gap-x-10">
               <p className="flex text-2xl font-medium">Phone</p>
-             <div className="flex gap-2"><UilHeadphones  size="20"/> +251-900-000</div>
+              <div className="flex gap-2">
+                <UilPhoneVolume size="20" /> +251-924-472-257
+              </div>
             </div>
-            <div className="flex flex-col ml-8 gap-4">
+            <div className="flex gap-x-10">
               <p className="flex text-2xl font-medium">Email</p>
-             <div className="flex gap-2"><UilEnvelopeOpen  size="20"/>service@email.com</div>
-             <div className="flex gap-2"><UilEnvelopeOpen  size="20"/>work@email.com</div>
-             <div className="flex gap-2"><UilEnvelopeOpen  size="20"/>branch@email.com</div>
-             
+              <div className="flex gap-x-10">
+                <div className="flex gap-2">
+                  <UilEnvelopeOpen size="20" />
+                  danielkassahun2@gmail.com
+                </div>
+                <div className="flex gap-2">
+                  <UilEnvelopeOpen size="20" />
+                  ephyaabb@gmail.com
+                </div>
+              </div>
             </div>
-            <div className="flex flex-col ml-20 gap-4">
+            <div className="flex gap-x-10">
               <p className="flex text-2xl font-medium">Address</p>
-             <div className="flex gap-2 ttext-sm font-thin">friendship mall, bole, Addis Ababa </div> 
+              <div className="flex gap-2 ttext-sm font-thin">
+                Friendship mall, bole, Addis Ababa{" "}
+              </div>
             </div>
           </div>
-          <div className="flex shadow-md mx-auto w-[550px] mt-24 px-8 py-4"> 
-            <form className="flex flex-col gap-6 ">
-              <div className="flex gap-3"> 
-                 <label className="flex flex-col relative gap-3  text-base text-gray-500">First Name
-                <div className="flex-1"><UilUserPlus size="20" className="absolute pointer-events-none top-12 text-black  left-3"/><input type="text" className=" px-12 w-full focus:outline-0 focus:border-2 focus:border-contact focus:ring-0 rounded-md border-[1px] border-gray-400 h-12" placeholder="Dani"/></div></label>
-                <label className="flex flex-col relative gap-3  text-base text-gray-500">Last Name
-                <div className="flex-1"><UilUserPlus size="20" className="absolute pointer-events-none top-12  text-black left-3"/><input type="text" className=" px-12 w-full   focus:outline-0 focus:border-2 focus:border-contact focus:ring-0 rounded-md border-[1px] border-gray-400 h-12" placeholder="Type your last name"/></div></label>
-
-              </div>
-              <div className="flex gap-3 w-full"> 
-                 <label className="flex flex-col relative gap-3  w-full text-base text-gray-500">Email
-                <div className="flex"><UilEnvelopeAlt size="20" className="absolute pointer-events-none top-12 text-black  left-3"/><input type="text" className=" px-12 w-full focus:outline-0 focus:border-2 focus:border-contact focus:ring-0 rounded-md border-[1px] border-gray-400 h-12" placeholder="service@email.com"/></div></label>
-               
-              </div>
-              <div className="flex gap-3 w-full"> 
-                 <label className="flex flex-col relative gap-3  w-full text-base text-gray-500">Phone number
-                <div className="flex"><UilPhoneVolume size="20" className="absolute pointer-events-none top-12 text-black  left-3"/><input type="text" className=" px-12 w-full  focus:outline-0 focus:border-2 focus:border-contact focus:ring-0 rounded-md border-[1px] border-gray-400 h-12" placeholder="+251-900-000"/></div></label>
-               
-              </div>
-              <div className="flex gap-3 w-full "> 
-                 <label className="flex flex-col relative gap-3  w-full text-base text-gray-500">Phone number
-                <div className="flex"><UilCommentAltLines size="20" className="absolute pointer-events-none top-12 text-black  left-3"/><textarea type="text" className=" px-12 w-full h-52  rounded-md border-[1px] border-gray-400 py-2 focus:outline-0 focus:border-2 focus:border-contact focus:ring-0" placeholder="Type Message"></textarea></div></label>
-               
-              </div>
-              <div className="flex "> 
-                <button className="flex bg-contact py-2 text-white px-8 shadow-md hover:shadow-xl rounded-lg">Send</button>
-              </div>
-            </form>
+          <div className="w-[90%]  rounded border-2 border-primary mr-10">
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={position}>
+                <Popup>
+                  {/* A pretty CSS3 popup. <br /> Easily customizable. */}
+                </Popup>
+              </Marker>
+            </MapContainer>
+            ,
           </div>
-       </div>
-       <div className="flex w-full  justify-center py-12">
-       <img src={address} className="flex  shadow-sm border-2 w-[60%]"/>
-       </div>
+        </div>
+        <div className=" flex mx-auto  w-[70%]">
+          <form className="flex flex-col gap-6 w-[550px] shadow-md  bg-white mt-24 px-12 py-20 rounded-xl ">
+            <div className="flex gap-3 ">
+              <label className="flex flex-col relative gap-3  text-base text-gray-500">
+                First Name
+                <div className="flex-1">
+                  <UilUserPlus
+                    size="20"
+                    className="absolute pointer-events-none top-12 text-black  left-3"
+                  />
+                  <input
+                    type="text"
+                    className=" px-12 w-full focus:outline-0 focus:border-2 focus:border-contact focus:ring-0 rounded-md border-[1px] border-gray-400 h-12"
+                    placeholder="First Name"
+                  />
+                </div>
+              </label>
+              <label className="flex flex-col relative gap-3  text-base text-gray-500">
+                Last Name
+                <div className="flex-1">
+                  <UilUserPlus
+                    size="20"
+                    className="absolute pointer-events-none top-12  text-black left-3"
+                  />
+                  <input
+                    type="text"
+                    className=" px-12 w-full   focus:outline-0 focus:border-2 focus:border-contact focus:ring-0 rounded-md border-[1px] border-gray-400 h-12"
+                    placeholder="Type your last name"
+                  />
+                </div>
+              </label>
+            </div>
+            <div className="flex gap-3 w-full">
+              <label className="flex flex-col relative gap-3  w-full text-base text-gray-500">
+                Email
+                <div className="flex">
+                  <UilEnvelopeAlt
+                    size="20"
+                    className="absolute pointer-events-none top-12 text-black  left-3"
+                  />
+                  <input
+                    type="text"
+                    className=" px-12 w-full focus:outline-0 focus:border-2 focus:border-contact focus:ring-0 rounded-md border-[1px] border-gray-400 h-12"
+                    placeholder="service@email.com"
+                  />
+                </div>
+              </label>
+            </div>
+            <div className="flex gap-3 w-full">
+              <label className="flex flex-col relative gap-3  w-full text-base text-gray-500">
+                Phone number
+                <div className="flex">
+                  <UilPhoneVolume
+                    size="20"
+                    className="absolute pointer-events-none top-12 text-black  left-3"
+                  />
+                  <input
+                    type="text"
+                    className=" px-12 w-full  focus:outline-0 focus:border-2 focus:border-contact focus:ring-0 rounded-md border-[1px] border-gray-400 h-12"
+                    placeholder="+251-900-000"
+                  />
+                </div>
+              </label>
+            </div>
+            <div className="flex gap-3 w-full ">
+              <label className="flex flex-col relative gap-3  w-full text-base text-gray-500">
+                Phone number
+                <div className="flex">
+                  <UilCommentAltLines
+                    size="20"
+                    className="absolute pointer-events-none top-12 text-black  left-3"
+                  />
+                  <textarea
+                    type="text"
+                    className=" px-12 w-full h-52  rounded-md border-[1px] border-gray-400 py-2 focus:outline-0 focus:border-2 focus:border-contact focus:ring-0"
+                    placeholder="Type Message"
+                  ></textarea>
+                </div>
+              </label>
+            </div>
+            <div className="flex ">
+              <button className="flex bg-contact py-2 text-white px-8 shadow-md hover:shadow-xl rounded-lg">
+                Send
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  ); 
+  );
 }
 export default Contact;
